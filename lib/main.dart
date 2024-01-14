69843c928c552f3bb03fca60edc0ceb7327d4d97
Talha2119 <talha.ismail2119@gmail.com>
2024-01-14 12:52:13 +0600

    splash_screen_implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
+
+  // Todo : Add Widgets Binding
+  // Todo : Init Local Storage
+  // Todo : Await Native Splash
+  // Todo : Initialize Firebase
+  // Todo : Initialize Authentication
+
+
   runApp(const MyApp());
 }
 
